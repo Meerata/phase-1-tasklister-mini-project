16ff713dd5fa1395f687d968967d3d98ef2a3e0a
Meerata <merata87@gmail.com>
2024-05-23 16:36:39 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const newTaskDescription = document.getElementById(
+      "new-task-description"
+    ).value;
+    if (newTaskDescription === "") return;
+
+    const newTask = document.createElement("li");
+    newTask.textContent = newTaskDescription;
+
+    // Delete button for each task
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+      taskList.removeChild(newTask);
+    });
+
+    newTask.appendChild(deleteButton);
+    taskList.appendChild(newTask);
+
+    // Clear the input field after adding the task
+    document.getElementById("new-task-description").value = "";
+  });
 });
